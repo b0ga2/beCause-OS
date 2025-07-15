289f3538595131c64f13e852f1b21c2aec33de4d
b0ga2 <137201883+b0ga2@users.noreply.github.com>
2025-07-15 10:27:04 +0100

    Update setup.sh
    Some extra commenting on the code

--- setup.sh
+++ setup.sh
@@ -7,8 +7,10 @@ set -xe
 # To assemble the assembly with gcc
 gcc -ffreestanding -O3 -nostdlib -c boot.s -o boot.o
 
-# Linking using the gcc, generating the file myos.bin using freestading,
-# 03 is optimization, nostdlib means no standard libs,
+# gcc - Linking using the gcc, 
+# --freestanding - Tells the compiler that the environment is freestanding (no standard library or OS)
+# -03 - enables aggressive optimizations for performance.
+# -nostdlib - Prevents linking against the standard libraries
 # currently only linking the boot.o 
 # -lgcc means using the linker from gcc
 gcc -T linker.ld -o myos.bin -ffreestanding -O3 -nostdlib boot.o -lgcc
@@ -19,7 +21,11 @@ cp myos.bin isodir/boot/myos.bin
 
 cp grub.cfg isodir/boot/grub/grub.cfg
 
-#
+# grub2-mkrescue - A tool that creates a bootable ISO image using GRUB2 as the bootloader
+# isodir - Directory containing the bootable files
 grub2-mkrescue -o myos.iso isodir
 
+# qemu-system-x86_64 - Launches QEMU for emulating a 64-bit x86 system.
+# -enable-kvm - Enables KVM (Kernel-based Virtual Machine) for performance
+# myos.iso - Tells QEMU to use myos.iso as the CD-ROM (bootable ISO) image.
 qemu-system-x86_64 -enable-kvm -cdrom myos.iso
